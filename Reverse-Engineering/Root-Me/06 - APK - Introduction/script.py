c257479aa2f6b9ba894a2f698bd7576254eadc29
Tranh A Nguyen <80273857+Trunganhlaptrinh@users.noreply.github.com>
2026-06-16 17:22:25 +0700

    update rootme 6

---
+++ Reverse-Engineering/Root-Me/06 - APK - Introduction/script.py
@@ -0,0 +1,23 @@
+def makeFlag(s):
+    a = "" + s[5]
+    _b = s[2] + ""
+    for s_ in range(len(s)):
+        b = _b[len(_b) - s_:] + _b[s_:]
+        if s_ >= 3:
+            _b2 = _b + s[s_ - 3]
+        else:
+            _b2 = _b + s[len(s) - (3 - s_)]
+        
+        if s_ >= len(_b2):
+            _b = _b2 + s[s_ - len(_b2)]
+        elif len(s) >= len(_b2) - s_:
+            _b = _b2 + s[len(s) - (len(_b2) - s_)]
+        else:
+            _b = _b2 + s[len(s) - ((len(_b2) - s_) - len(s))]
+        
+        a = a + b[(((len(s) + len(_b)) * s_) + len(_b)) % len(b)]
+    
+    return a[0:2] + s[3] + a[3] + '0' + a[5:7]
+
+seed = "1dndr@"
+print(makeFlag(seed))
